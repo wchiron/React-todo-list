ef931ff86258227344df1712894627e2edf3423b
Wei Chiron <wei.chiron@gmail.com>
2019-08-11 21:28:59 +0200

    Change links to use api in Heroku

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import AddTodo from './components/AddTodo';
 import About from './components/pages/About';
 import axios from 'axios'; // it's a http library to replace the js http request such as fetch
 
+const linkToAPIHeroku = "https://todo-list-api-wchiron.herokuapp.com";
+
 class App extends Component {
   state = { // put the todos in the state/data in the app.js, so all the component files could have access to this state. Like data in the app in the components in Vue.jsß
     todos: [
@@ -32,14 +34,14 @@ class App extends Component {
 
   componentDidMount() { //is invoked immediately after a component is mounted (inserted into the tree)
     // axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5') // without the query parameter ?_limit=5, it will get the whole list of 200 todolist
-    axios.get("http://localhost:3000/todo") // use the api on the port 3000 on my own laptop
+    axios.get(linkToAPIHeroku + "/todo") // use the api on the port 3000 on my own laptop
       .then(res => this.setState({ todos: res.data })) // receive result from l25 in index.js, send the todos received from jsonplaceholder/api to the array
   }
 
   // toggle complete everyTodo
   markComplete = (id) => { // receive this markComplete through the props from TodoItem.js then Todos.js as the array of items are in this file. 
     const itemToToggle = this.state.todos.find( todo => todo.id === id);
-    axios.patch(`http://localhost:3000/todo/${id}`, {
+    axios.patch(`${linkToAPIHeroku}/todo/${id}`, {
       completed: !itemToToggle.completed, //for the item of this id, toggle it's completed value in the database
     })
       .then(res => {
@@ -56,7 +58,7 @@ class App extends Component {
 
   // delete todo, same logic as the markComplete, get the function through the props from TodoItem.js with id as parameter
   delTodo = (id) => {
-    axios.delete(`http://localhost:3000/todo/${id}`) // using back ticks here instead of single quote to be able to use the dollar sign for the id
+    axios.delete(`${linkToAPIHeroku}/todo/${id}`) // using back ticks here instead of single quote to be able to use the dollar sign for the id
       .then(res => this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)]}));
   }
 
@@ -67,7 +69,7 @@ class App extends Component {
     //   title,  // this here actually means title: title. In ES6, if the title value equals title, can only write title all short
     //   completed: false,
     // }
-    axios.post('http://localhost:3000/todo', {
+    axios.post(linkToAPIHeroku + "/todo", {
       // id: uuid.v4(), // this is not working as jsonplacefolder will erase my id and replace it with 201, which is their id, that's the reason to change the id when receive the response
       title,
     })
